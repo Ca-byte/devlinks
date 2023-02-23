db2930ee8194852ca6c4b5d2915437e52a9fc550
Caroline Vieira <carolinesanvieira@gmail.com>
2023-02-23 20:50:59 +0100

    feat: functionality - Switch - replace Avatar

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode(){
+	const html = document.documentElement
+	html.classList.toggle("light")
+	const img = document.querySelector("#profile img")
+	const alt = document.querySelector("#profile img")
+
+if(html.classList.contains("light")){
+		img.setAttribute("src", "./assets/avatar-ligh.png")
+		alt.setAttribute("alt", "Caroline Vieira profile picture she is with sunglasses in a florest beside a wild dog")
+	} else {
+		img.setAttribute("src", "./assets/avatar-dark.png")
+		alt.setAttribute("alt", "Caroline Vieira profile picture in black shirt and purple coat")
+	}
+}
