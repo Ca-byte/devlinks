b1d90b8d73f1f37fb077dab9f2a0fec100cd61d7
Caroline Vieira <carolinesanvieira@gmail.com>
2023-02-23 21:03:56 +0100

    feat: links - project links and container border

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleMode(){
 	const alt = document.querySelector("#profile img")
 
 if(html.classList.contains("light")){
-		img.setAttribute("src", "./assets/avatar-ligh.png")
+		img.setAttribute("src", "./assets/avatar-light.png")
 		alt.setAttribute("alt", "Caroline Vieira profile picture she is with sunglasses in a florest beside a wild dog")
 	} else {
 		img.setAttribute("src", "./assets/avatar-dark.png")
